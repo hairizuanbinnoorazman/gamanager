a1ce0956e694aeb20f4ecf89fbc767e43f4c618d
Hairizuan <hairizuanbinnoorazman@gmail.com>
2017-03-07 01:54:48 +0800

    feat: Add feature to allow user to search for functions under package name by typing ?packageName

---
+++ R/gamanager.R
@@ -0,0 +1,5 @@
+#' gamanager: Managing Google Analytics Management Settings via R
+#' @description Quick way to manage Google Analytics Managements settings with the gamanager package
+#' @docType package
+#' @name gamanager
+NULL
